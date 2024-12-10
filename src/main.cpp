8727fb477fa8254ce8b092edd27dcea02b9052ad
Race Williams <race@racewilliams.com>
2024-12-10 14:39:41 -0500

    impl a lot for parser errors, printer, etc. see todos

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,33 @@
 #include "parser.hpp"
+#include "version.hpp"
 #include <CLI/CLI.hpp>
-#include <fstream>
 #include <iostream>
 #include <string>
 
 int main(int argc, char **argv) {
   CLI::App app{"a command-line HTTP client"};
 
-  app.add_flag("-v,--version", "print version and exit");
+  app.set_version_flag("--version", std::string(RAQUEST_VERSION));
+
   std::string file_url;
-  app.add_option("pos1,-f,--file", file_url, "specify a .raq file")->required()->check(CLI::ExistingFile);
+  app.add_option("pos1,-f,--file", file_url, "specify a .raq file")
+      ->required()
+      ->check(CLI::ExistingFile);
 
   CLI11_PARSE(app, argc, argv);
 
   try {
-    std::ifstream file(file_url);
-    std::string file_as_str((std::istreambuf_iterator<char>(file)),
-                            std::istreambuf_iterator<char>());
-    Parser parser = Parser(file_as_str);
+    Parser parser = Parser(file_url);
     auto request = parser.parse();
-    if (!request.has_value()) {
-      for (auto error : parser.get_errors()) {
-        std::cerr << error.message << std::endl;
+    if (!parser.errors.empty()) {
+      // TODO: print error head
+      for (ParserError error : parser.errors) {
+        error.print();
       }
+      // TODO: print error foot
+    } else {
+      request->execute();
     }
-    request->execute();
   } catch (const std::exception &ex) {
     std::cerr << "error: " << ex.what() << std::endl;
     return 1;
