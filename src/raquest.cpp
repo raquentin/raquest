e067c51c546a939ba0c15ff74fe72c2ba40b27ec
Race Williams <race@raquent.in>
2025-02-12 22:34:45 -0500

    Raquest::run returns a CurlResponse on success

--- src/raquest.cpp
+++ src/raquest.cpp
@@ -5,42 +5,44 @@
 
 Raquest::Raquest(const std::string &file_name) : file_name_(file_name) {}
 
-std::vector<std::unique_ptr<Error>> Raquest::run() const {
+std::expected<CurlResponse, std::vector<std::unique_ptr<Error>>>
+Raquest::run() const {
     Parser parser(file_name_);
+    std::vector<std::unique_ptr<Error>> errors;
+
+    // parser.parse()
     const auto parse_result = parser.parse();
     if (!parse_result) [[unlikely]] {
         const auto &parser_errors = parse_result.error();
 
-        std::vector<std::unique_ptr<Error>> errors;
         errors.reserve(parser_errors.size());
         for (auto &pe : parser_errors) {
             errors.push_back(std::make_unique<ParserError>(pe));
         }
-        return errors;
+        return std::unexpected(std::move(errors));
     }
 
+    // curl_request.execute()
     const auto [curl_request, assertion_set] = parse_result.value();
-
     const auto curl_result = curl_request.execute();
     if (!curl_result) [[unlikely]] {
-        std::vector<std::unique_ptr<Error>> errors;
         errors.push_back(std::make_unique<CurlError>(curl_result.error()));
-        return errors;
+        return std::unexpected(std::move(errors));
     }
 
-    const auto curl_response = curl_result.value();
+    // validate(assertions)
+    const CurlResponse curl_response = curl_result.value();
     if (assertion_set.has_value()) {
         const auto failed_assertions =
             validate(curl_response, assertion_set.value());
         if (!failed_assertions.empty()) [[unlikely]] {
-            std::vector<std::unique_ptr<Error>> errors;
             errors.reserve(failed_assertions.size());
             for (auto &fa : failed_assertions) {
                 errors.push_back(std::make_unique<AssertionError>(fa));
             }
-            return errors;
+            return std::unexpected(std::move(errors));
         }
     }
 
-    return std::vector<std::unique_ptr<Error>>{};
+    return std::move(curl_response);
 }
